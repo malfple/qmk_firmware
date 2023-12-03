83f782aa6206b9ccc39261b49ab1eb1100b970be
Richard Goulter <richard.goulter@gmail.com>
2023-12-03 11:49:33 +0700

    [Layout] add split_3x5_3 rgoulter-basic

---
+++ layouts/community/split_3x5_3/rgoulter-basic/rules.mk
@@ -0,0 +1,9 @@
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+ENCODER_ENABLE = no
+RGB_MATRIX_ENABLE = no
+OLED_ENABLE = no
+
+USER_NAME := rgoulter
+
+include layouts/community/split_3x5_3/rgoulter/options.mk

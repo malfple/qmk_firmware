6dd35e78fa31d6ece01bd4ab71bbb28b6acc0f42
Richard Goulter <richard.goulter@gmail.com>
2023-12-03 11:48:44 +0700

    [Layout] add split_3x5_3 rgoulter

---
+++ layouts/community/split_3x5_3/rgoulter/rules.mk
@@ -0,0 +1,12 @@
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+ENCODER_ENABLE = yes
+OLED_ENABLE = yes
+
+USER_NAME := rgoulter
+
+HAPTIC_ENABLE = yes
+HAPTIC_DRIVER += solenoid
+SOLENOID_ENABLE = yes
+
+include layouts/community/split_3x5_3/rgoulter/options.mk
